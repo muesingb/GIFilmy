64c9a16c10788abbce6e61e39e5169a8e129e94e
Barbara <muesingb@gmail.com>
2019-11-12 14:36:44 -0500

    able to change to next_question

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,30 +1,52 @@
 class GamesController < ApplicationController
     def index
     end
-    
-    def show
-        #Game.start_game
-        @game = Game.find(params[:id])
-        session[:answer] = @game.questions.first.title
+
+    def game_questions
+        @game = Game.find(params[:game_id])
+        @question = @game.questions.find(params[:id])
+
+        render :'questions/show'
     end
 
-    def answer
-        byebug
-        #flash[:correct] == question.title
-        #if game_params[:answer] == question.title
-        # if game_params[:answer] == session[:answer]
-        #     if session[:score]
-        #       session[:score] = session[:score] + 100
-        #     else
-        #       session[:score] = 0
-        #     end
-        # end
-        # redirect_to '/'
+    def get_answer
+        #adds to score, redirects to answer page, 
+        #answer page will redirect to next question's show page
+        if game_params[:answer] == question_params[:title]
+            flash[:feedback] = "Right!"
+            if session[:score]
+                session[:score] = session[:score] + 100
+            else
+                session[:score] = 0
+            end
+        else flash[:feedback] = "Wrong!"
+        end
+
+        flash[:correct] = question_params[:title]
+        #flash[:game_id] = game_params[:game_id]
+        #flash[:next_question] = (question_params[:question_id]).to_i + 1
+        flash[:game] = Game.find(game_params[:game_id])
+        flash[:question] = Question.find(question_params[:question_id])
+       
+        redirect_to :action => "show_answer"
+    end
+
+    def show_answer
+        #@game_id = flash[:game_id]
+        @next_question = (flash[:question].values.first.to_i) + 1
+        @game_id = flash[:game].values.first
+        @question = flash[:question]
+        render :'questions/answer'
+        #redirect_to question_params[:id]+1
     end
 
 private
 
     def game_params
         params.require(:game).permit(:answer, :game_id, :score)
+    end
+
+    def question_params
+        params.require(:question).permit(:title, :question_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 root to: 'games#index'
 resources :users 
 resources :games
-post '/answer', to: 'games#answer'
+post '/get_answer', to: 'games#get_answer'
+get '/show_answer', to: 'games#show_answer'
+get '/game/:game_id/question/:id', to: 'games#game_questions'
+resources :questions
 end
