d9de55c6668fbf270bd352c9a438396a69303896
github username <github email address>
2019-11-13 18:28:28 -0500

    login stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,28 @@ class SessionsController < ApplicationController
 
     def create
       session[:username] = params[:username] 
-      redirect_to '/'
+      redirect_to user_path(User.find_by(name: session["username"]))  
     end 
 
     def destroy
       session.delete :username
     end
 end
+
+
+
+# params =  {
+#     "id": 2,
+#     "cool stuff": "At Clarke Campus"
+# }
+
+# user2 = User.find(params["id"])  
+
+# employee1 = {
+#     "id": 1
+#     "name": "spongebob",
+#     "employer": "Krusty Krab",
+#     "boss": "Mr. Crabs"
+# }
+
+# employee1["name"] => "spongebob"
