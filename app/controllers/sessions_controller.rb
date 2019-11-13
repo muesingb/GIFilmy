a430e431d9825a1a8586f7b03822b3776d26a6d0
github username <github email address>
2019-11-13 16:55:56 -0500

    working on games index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
+    
     def new
     end
 
     def create
-      session[:username] = params[:username]
+      session[:username] = params[:username] 
       redirect_to '/'
     end 
 
