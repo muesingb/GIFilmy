0baf777f8e5e90e3ee1e508eb254722ca28783c3
Barbara <muesingb@gmail.com>
2019-11-13 11:14:57 -0500

    fixed create user controller action and endgame

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,13 @@
 class SessionsController < ApplicationController
+    def new
+    end
 
-def new
-end
+    def create
+      session[:username] = params[:username]
+      redirect_to '/'
+    end 
 
-def create
-    session[:username] = params[:username]
-    redirect_to '/'
-end 
-
-def destroy
-    session.delete :username
-  end
-
+    def destroy
+      session.delete :username
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,8 @@ resources :questions
 post '/answer', to: 'games#answer'
 get '/show_answer', to: 'games#show_answer'
 get '/game/:game_id/question/:id', to: 'games#game_questions'
-resources :questions
+get '/game/end', to: 'games#end_game'
 get '/login', to: 'sessions#new'
 post '/login', to: 'sessions#create'
 post '/logout', to: 'sessions#destroy'
-get '/game/end', to: 'games#end_game'
 end
